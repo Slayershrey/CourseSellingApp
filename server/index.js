1095fede4fd339ab6fd89c083b09bc663afe2621
Slayershrey <sawalkar336@gmail.com>
2024-01-23 11:13:35 +0530

    added dbstring from .env

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ require("dotenv").config()
 
 const app = express()
 const port = process.env.PORT
+const dbstring = process.env.DATABASE_URL
 
 app.use(cors())
 app.use(express.json())
@@ -14,14 +15,11 @@ app.use(express.json())
 app.use("/admin", adminRouter)
 app.use("/user", userRouter)
 
-mongoose.connect(
-  "mongodb+srv://Shreyansh:sunny@cluster0.ygjtchm.mongodb.net/",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    dbName: "CourseSellingApp",
-  }
-)
+mongoose.connect(dbstring, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: "CourseSellingApp",
+})
 
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${port}`)
